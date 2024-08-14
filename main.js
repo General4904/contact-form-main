03708cbc1f83192b4206873b10491b917da40884
Generalissimo <koyejoibukun@gmail.com>
2024-08-14 14:41:23 +0100

    Completed project  Changes to be committed: 	modified:   index.html 	modified:   main.js 	modified:   styles/styles.css

--- main.js
+++ main.js
@@ -1,42 +1,139 @@
 // Script goes here
-console.log ('Hello World');
+console.log("Hello World");
 
-const theForm = document.getElementById('contact');
-const firstname = document.getElementById('first');
-const lastname = document.getElementById('last');
-const listItem = document.getElementById('first-name-error');
-const secondListItem = document.getElementById('last-name-error');
-const newPara = document.createElement('p');
+const theForm = document.getElementById("contact");
+const firstname = document.getElementById("first");
+const lastname = document.getElementById("last");
+const listItem = document.getElementById("first-name-error");
+const secondListItem = document.getElementById("last-name-error");
+const mail = document.getElementById("mail");
+const newPara = document.createElement("p");
+const _mail = document.getElementById("mailing");
+const radioCheck = document.getElementById("radio");
+const message = document.getElementById("messageSent");
+const messageSection = document.getElementById("message");
+const lastSection = document.getElementById("consent");
+newPara.style.display = "block";
 
-theForm.addEventListener('submit', function (event){
-    event.preventDefault();
+theForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  validateForm();
+});
 
-    const nameInput = document.getElementsByClassName('name-field');
-    let valid = true;
-    const namePattern = /^[A-Za-z]+$/;
+function validateForm() {
+  // Firstname Validation
+  const nameValue = firstname.value;
+  if (nameValue === "") {
+    firstname.scrollIntoView({ behavior: "smooth" });
+    firstname.style.borderColor = "red";
+    listItem.appendChild(newPara);
+    newPara.style.color = "red";
+    newPara.innerHTML = "This field is required";
+    return false;
+  } else if (nameValue.length < 3) {
+    firstname.scrollIntoView({ behavior: "smooth" });
+    firstname.style.borderColor = "red";
+    listItem.appendChild(newPara);
+    newPara.style.color = "red";
+    newPara.innerHTML = "Firstname must be more than three characters";
+    return false;
+  }
 
-    if (firstname.value === '' || firstname !== namePattern) {
-        listItem.appendChild(newPara);
-        newPara.innerHTML = `This field is required`;
-        newPara.style.color = `red`;
-        newPara.style.display = `block`;
-        firstname.focus();
+  // Lastname validation
+  const secondname = lastname.value;
+  if (secondname === "") {
+    lastname.scrollIntoView({ behavior: "smooth" });
+    lastname.style.borderColor = "red";
+    secondListItem.appendChild(newPara);
+    newPara.style.color = "red";
+    newPara.innerHTML = "This field is required";
+    return false;
+  } else if (secondname.length < 3) {
+    lastname.scrollIntoView({ behavior: "smooth" });
+    lastname.style.borderColor = "red";
+    secondListItem.appendChild(newPara);
+    newPara.style.color = "red";
+    newPara.innerHTML = "Firstname must be more than three characters";
+    return false;
+  }
+
+  // Email Validation
+  const mailValue = mail.value;
+  const mailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+  if (mailValue === "") {
+    mail.scrollIntoView({ behavior: "smooth" });
+    mail.style.borderColor = "red";
+    _mail.appendChild(newPara);
+    newPara.style.color = "red";
+    newPara.innerHTML = "Email is required";
+    return false;
+  } else if (!mailPattern.test(mailValue)) {
+    mail.scrollIntoView({ behavior: "smooth" });
+    mail.style.borederColor = "red";
+    _mail.appendChild(newPara);
+    newPara.style.color = "red";
+    newPara.innerHTML = "Please enter a valid email";
+    return false;
+  }
+
+  // Radio button validation
+  const radioButton = document.getElementsByName("query");
+  let querySelected = false;
+  for (let i = 0; i < radioButton.length; i++) {
+    if (radioButton[i].checked) {
+      querySelected = true;
+      break;
     }
+  }
 
-    // else if (lastname.value === ''){
-    //     secondListItem.appendChild(newPara);
-    //     newPara.innerHTML = `This field is required`;
-    //     newPara.style.color = `red`;
-    //     newPara.style.display = `block`;
-    //     lastname.focus();
-    // }
+  if (!querySelected) {
+    radioCheck.appendChild(newPara);
+    newPara.style.color = "red";
+    newPara.innerHTML = "Please select a query";
+    radioCheck.scrollIntoView({ behavior: "smooth" });
+    return false;
+  }
 
-    else {
-        alert(`Success`)
+  // Message section validation
+  const messageValue = messageSent.value;
+  if (messageValue === "") {
+    messageSent.scrollIntoView({ behavior: "smooth" });
+    messageSent.style.borderColor = "red";
+    messageSection.appendChild(newPara);
+    newPara.style.color = "red";
+    newPara.innerHTML = "This field is required";
+    return false;
+  }
+
+  // Checkbox section
+  const checkbox = document.getElementsByName("consent");
+  let checkboxSelected = false;
+
+  for (let i = 0; i < checkbox.length; i++) {
+    if (checkbox[i].checked) {
+      checkboxSelected = true;
+      break;
     }
-})
+  }
 
-                
+  if (!checkboxSelected) {
+    lastSection.appendChild(newPara);
+    newPara.style.color = "red";
+    newPara.innerHTML = `To submit this form, please consent to being contacted`;
+    return false;
+  }
+
+  window.scrollTo({ top: 0, behavior: "smooth" });
+
+  showDialog();
+  theForm.onsubmit();
+}
+
+function showDialog() {
+  let show = document.getElementById("scroll");
+  let dialog = document.getElementById("overlay");
+  dialog.style.display = "block";
+}
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /*
@@ -53,12 +150,12 @@ document.getElementById('nameForm').addEventListener('submit', function(event) {
         if (value === '') {
             valid = false;
             alert(`Name ${i + 1} is required.`);
-            input.focus();
+            input.scrollIntoView({ behavior: "smooth" });
             break;
         } else if (!namePattern.test(value)) {
             valid = false;
             alert(`Name ${i + 1} must contain only letters.`);
-            input.focus();
+            input.scrollIntoView({ behavior: "smooth" });
             break;
         }
     }
@@ -71,13 +168,6 @@ document.getElementById('nameForm').addEventListener('submit', function(event) {
 });
 */
 
-
-
-
-
-
-
-
 function test() {
-    alert('Hello World')
+  alert("Hello World");
 }
